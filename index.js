175fe07d727613aa4f01cd2897505803e980949e
Felipe Oliveira <oliveira.felipe28@gmail.com>
2022-02-11 14:49:56 -0300

    criado login mobile

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const hamburguerMenu = $('.lineMenuDiv')
 hamburguerMenu.on('click', function(){
     const navMenu = $('.navMenuDiv ul')
     const displayNav = navMenu.css('display')
+    const loginButton = $('.listaLogin')
     if (displayNav == 'none'){
-        navMenu.append("<li><a>Login </a></li>")
         navMenu.css('display', 'flex')
         navMenu.css('background-color', '#0d2952a6')
+        loginButton.css('display', 'inline-block')
     } else{
         navMenu.css('display', 'none')
-        $('.navMenuDiv li:last-child').remove()
+        loginButton.css('display', 'none')
     }
 })
